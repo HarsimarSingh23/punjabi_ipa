1232c25b1d70fe0d1545ad0e63489b35718d9885
harsimarsingh <sainiharsimar@gmail.com>
2024-05-20 11:11:31 +0530

    REFACTOR | IPA table

--- table.py
+++ table.py
@@ -2,27 +2,30 @@ HTML = """
 <table class="tg">
 <thead>
   <tr>
-    <th class="tg-0lax" colspan="5"><center>IPA TABLE<center></th>
+    <th class="tg-0lax" colspan="7"><center>IPA TABLE<center></th>
   </tr>
 </thead>
 <tbody>
   <tr>
     <td class="tg-0lax">Gurmukhi Word</td>
-    <td class="tg-0pky">WIKI</td>
-    <td class="tg-0pky">ShabadOS</td>
-    <td class="tg-0pky">HSGILL</td>
-    <td class="tg-0lax">Wiki IPA Sound</td>
+    <td class="tg-0pky" colspan="2" style="text-align:center">WIKI</td>
+    <td class="tg-0pky" colspan="2" style="text-align:center">ShabadOS</td>
+    <td class="tg-0pky" colspan="2" style="text-align:center">HSGILL</td>
   </tr>
   <tr>
     <td class="tg-0lax">ਅ </td>
     <td class="tg-0pky"><span style="font-weight:400;font-style:normal">ə</span></td>
+    <td class="tg-0pky"><audio controls> <source src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Mid-central_vowel.ogg" type=audio/ogg> </audio></td>
     <td class="tg-0pky"><span style="font-weight:400;font-style:normal">ə</span></td>
+    <td class="tg-0pky"><audio controls> <source src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Mid-central_vowel.ogg" type=audio/ogg> </audio></td>
     <td class="tg-0pky"><span style="font-weight:400;font-style:normal">ə</span></td>
-    <td class="tg-0pky"><span style="font-weight:400;font-style:normal"><audio controls> <source src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Mid-central_vowel.ogg" type=audio/ogg> </audio></span></td>
+    <td class="tg-0pky"><audio controls> <source src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Mid-central_vowel.ogg" type=audio/ogg> </audio></td>
     
   </tr>
   <tr>
     <td class="tg-0lax"></td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
     <td class="tg-0pky"></td>
     <td class="tg-0pky"></td>
     <td class="tg-0pky"></td>
